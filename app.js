92c16586126afe6b6cbf891f8b68ea043cb61e7c
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-05-03 18:45:41 +0500

    Brith day And Office Attendance

---
+++ app.js
@@ -0,0 +1,61 @@
+// Date.getTime()
+// Jan 1 1970 se abhe tk jitne milleseconds guzar gae wo lekar aajata he..
+
+// ------------aje ab tak kitne(second munit hour days mounth year ) guzar chuke hain check krne ka tarika---------
+
+// var now = new Date();
+// var bd = new Date("12/3/1999");  //mm/dd/yyyy
+// var milleseconds = now.getTime();
+// var bdMillseconds = bd.getTime();
+// var diff = milleseconds - bdMillseconds;
+// console.log("milleseconds=>", diff);
+// console.log("seconds=>", Math.round(diff / 1000));
+// console.log("minutes=>", Math.round(diff / 1000 / 60));
+// console.log("hours=>", Math.round(diff / 1000 / 60 / 60));
+// console.log("day=>", Math.round(diff / 1000 / 60 / 60 / 24));
+// console.log("months=>", Math.round(diff / 1000 / 60 / 60 / 24 / 30));
+// console.log("years=>", Math.round(diff / 1000 / 60 / 60 / 24 / 30 / 12));
+
+// <------------------------------------>
+
+
+
+
+
+// --------------office Attendance ka method banane ka tarika----------------
+
+// var attendanceTime = new Date();
+// attendanceTime.setHours(18);
+// attendanceTime.setMinutes(45);
+// attendanceTime.setSeconds(0);
+// console.log(attendanceTime);
+// var now = new Date();
+
+// var diff = now.getTime() - attendanceTime.getTime();
+// console.log(diff);
+// var minutes = Math.floor(diff / 1000 / 60);
+// console.log("minutes=>", minutes);
+// if (minutes <= 0) {
+//   console.log("You are on time.Keep it Up!");
+// } else if (minutes <= 5) {
+//   console.log("Bach gae aaj. Kal se fix 45 se pehle aajana");
+// } else {
+//   console.log("You are late.Salary kat gye.Yeah!");
+// }
+
+// <------------------------------->
+
+
+
+
+// -------------------------Counter Assignment Start-----------------
+
+
+
+
+
+
+
+
+
+// <--------------------->
